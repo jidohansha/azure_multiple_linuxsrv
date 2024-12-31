2bafbad5b19bdf0cd55596db6a2407278a23111e
Ant Morales <100240691+jidohansha@users.noreply.github.com>
2024-12-31 17:39:18 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,20 @@
+# root module for deployment of resources.
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.93.0" # version of the azure provider API
+    }
+  }
+
+  #  required_version = ">= 1.1.0" # version of terraform used
+}
+
+# credentials to access azure to provision resources
+provider "azurerm" {
+  features {}
+
+  subscription_id = "xxxxxxxx-xxxx"
+  tenant_id       = "xxxxxxxx-xxxx"
+}
